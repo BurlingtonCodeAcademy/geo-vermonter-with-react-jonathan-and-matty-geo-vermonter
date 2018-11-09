d78bb72b225806b13906ac1510598b0b3761540d
Matty Smith <matthewcsmith18@gmail.com>
2018-11-09 13:23:38 -0500

    Winner Functionality

---
+++ counties.js
@@ -0,0 +1,46 @@
+const counties = 
+[
+  {name: "Addison County", 
+center: [44.001944, -73.145556],
+town: "Middlebury"
+}, {name: "Bennington County",
+center: [43.1410, -73.0810],
+town: "Manchester Center"
+}, {name: "Caledonia County",
+center: [44.46, -72.1159899],
+town: "Danville"
+}, {name: "Chittenden County",
+center: [44.501944, -73.093889],
+town: "Essex Junction"
+}, {name: "Essex County",
+center: [44.73, -71.7801148],
+town: "Ferdinand"
+}, {name: "Franklin County",
+center: [44.809722, -73.087222],
+town: "St. Albans City"
+}, {name: "Grand Isle County",
+center: [44.7882463, -73.2909557],
+town: "North Hero"
+}, {name: "Lamoille County",
+center: [44.599563, -72.6278814],
+town: "Hyde Park"
+}, {name: "Orange County",
+center: [43.9956247, -72.3912821],
+town: "Chelsea"
+}, {name: "Orleans County",
+center: [44.83, -72.25],
+town: "Irasburg"
+}, {name: "Rutland County",
+center: [43.606944, -72.974722],
+town: "Rutland"
+}, {name: "Washington County",
+center: [44.262222, -72.580833],
+town: "Montpelier"
+}, {name: "Windham County",
+center: [42.984806, -72.656049],
+town: "Newfane"
+}, {name: "Windsor County",
+center: [43.484167, -72.385556],
+town: "Windsor"
+}
+];
--- src/index.js
+++ src/index.js
@@ -5,177 +5,213 @@ import './styles.css';
 import countyBorders from './countyBorders.js';
 import counties from './counties.js';
 
+function WarningBanner(props) {
+	if (!props.warn) {
+		return null;
+	}
+
+	return <div className="warning">Winner!</div>;
+}
+
 class Livemap extends React.Component {
-  constructor(props) {
-    super(props);
-    this.mapRef = React.createRef();
-    this.map = null;
-    this.lat = 43.7;
-    this.long = -72.6;
-    this.viewLat = this.lat;
-    this.viewLong = this.long;
+	constructor(props) {
+		super(props);
+		this.mapRef = React.createRef();
+		this.map = null;
+		this.lat = 43.7;
+		this.long = -72.6;
+		this.viewLat = this.lat;
+		this.viewLong = this.long;
+		this.state = { showWarning: true };
+		this.handleToggleClick = this.handleToggleClick.bind(this);
+	}
+	componentDidMount() {
+		this.map = Leaflet.map(this.mapRef.current, {
+			zoomControl: false
+		}).setView([this.lat, this.long], 15);
+		Leaflet.tileLayer(
+			'http://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}',
+			{
+				attribution:
+					'&copy; <a href="http://www.esri.com/">Esri</a>, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community',
+				maxZoom: 18,
+				minZoom: 1,
+				center: [this.lat, this.long]
+			}
+		).addTo(this.map);
+		Leaflet.geoJSON(countyBorders, {
+			color: 'rgb(255, 255, 255, 0.5)',
+			fillOpacity: '.175',
+			weight: 2
+		}).addTo(this.map);
+		this.map.doubleClickZoom.disable();
+		this.map.scrollWheelZoom.disable();
+		this.map.boxZoom.disable();
+		this.map.keyboard.disable();
+		this.map.dragging.disable();
+		this.map.touchZoom.disable();
 
-  }
-  componentDidMount() {
-    this.map = Leaflet.map(this.mapRef.current, {
-      zoomControl: false
-    }).setView([this.lat, this.long], 15);
-    Leaflet.tileLayer(
-      'http://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}',
-      {
-        attribution:
-          '&copy; <a href="http://www.esri.com/">Esri</a>, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community',
-        maxZoom: 18,
-        minZoom: 1,
-        center: [this.lat, this.long]
-      }
-    ).addTo(this.map);
-    Leaflet.geoJSON(countyBorders, {
-      color: 'rgb(255, 255, 255, 0.5)',
-      fillOpacity: '.175',
-      weight: 2
-    }).addTo(this.map);
-    this.map.doubleClickZoom.disable();
-    this.map.scrollWheelZoom.disable();
-    this.map.boxZoom.disable();
-    this.map.keyboard.disable();
-    this.map.dragging.disable();
-    this.map.touchZoom.disable();
+		this.map.on('click', this.onMapClick);
+	}
 
-    this.map.on('click', this.onMapClick);
-  }
+	componentWillUnmount() {
+		this.map.off('click', this.onMapClick);
+		this.map = null;
+	}
 
-  componentWillUnmount() {
-    this.map.off('click', this.onMapClick);
-    this.map = null;
-  }
+	handleToggleClick() {
+		this.setState(prevState => ({
+			showWarning: !prevState.showWarning
+		}));
+	}
 
-  randomCountyIndex() {
-    let index = (Math.floor(Math.random() * counties.length));
-    return index;
-  }
+	randomCountyIndex() {
+		let index = Math.floor(Math.random() * counties.length);
+		return index;
+	}
 
-  moveAndDrawLine(latPlus, longPlus, color) {
-    let pointA = new Leaflet.LatLng(this.viewLat, this.viewLong);
-    this.viewLat = +this.viewLat + latPlus;
-    this.viewLong = +this.viewLong + longPlus;
-    let pointB = new Leaflet.LatLng(this.viewLat, this.viewLong);
-    let pointList = [pointA, pointB];
+	moveAndDrawLine(latPlus, longPlus, color) {
+		let pointA = new Leaflet.LatLng(this.viewLat, this.viewLong);
+		this.viewLat = +this.viewLat + latPlus;
+		this.viewLong = +this.viewLong + longPlus;
+		let pointB = new Leaflet.LatLng(this.viewLat, this.viewLong);
+		let pointList = [pointA, pointB];
 
-    let myPolyline = new Leaflet.polyline(pointList, {
-      color: color,
-      weight: 4,
-      opacity: 0.7,
-      smoothFactor: 1
-    });
+		let myPolyline = new Leaflet.polyline(pointList, {
+			color: color,
+			weight: 4,
+			opacity: 0.7,
+			smoothFactor: 1
+		});
 
-    myPolyline.addTo(this.map);
-    this.map.panTo(new Leaflet.LatLng(this.viewLat, this.viewLong));
-  }
+		myPolyline.addTo(this.map);
+		this.map.panTo(new Leaflet.LatLng(this.viewLat, this.viewLong));
+	}
 
-  goNorth() {
-    this.moveAndDrawLine(0.0025, 0, 'yellow');
-  }
+	goNorth() {
+		this.moveAndDrawLine(0.0025, 0, 'yellow');
+	}
 
-  goWest() {
-    this.moveAndDrawLine(0, -0.0025, 'cyan')
-  }
+	goWest() {
+		this.moveAndDrawLine(0, -0.0025, 'cyan');
+	}
 
-  goEast() {
-    this.moveAndDrawLine(0, 0.0025, 'orange')
-  }
+	goEast() {
+		this.moveAndDrawLine(0, 0.0025, 'orange');
+	}
 
-  goSouth() {
-    this.moveAndDrawLine(-0.0025, 0, 'red');
-  }
+	goSouth() {
+		this.moveAndDrawLine(-0.0025, 0, 'red');
+	}
 
-  goReturn() {
-    this.moveAndDrawLine.bind(this);
-    this.viewLat = this.lat;
-    this.viewLong = this.long;
-    this.map.flyTo(new Leaflet.LatLng(this.lat, this.long));
-  }
+	goReturn() {
+		this.moveAndDrawLine.bind(this);
+		this.viewLat = this.lat;
+		this.viewLong = this.long;
+		this.map.flyTo(new Leaflet.LatLng(this.lat, this.long));
+	}
 
-  randomCounty() {
-    let countyIndex = this.randomCountyIndex();
-    this.lat = counties[countyIndex].center[0];
-    this.long = counties[countyIndex].center[1];
-    this.goReturn();
-  }
+	randomCounty() {
+		let countyIndex = this.randomCountyIndex();
+		this.lat = counties[countyIndex].center[0];
+		this.long = counties[countyIndex].center[1];
+		this.goReturn();
+	}
 
-  onMapClick = e => {
-    //const { lat, lng } = e.latlng;
-    //  Leaflet.marker([lat, lng]).addTo(this.map)
-  };
+	onMapClick = e => {
+		//const { lat, lng } = e.latlng;
+		//  Leaflet.marker([lat, lng]).addTo(this.map)
+	};
 
-  render() {
-    return (
-      <div className="row">
-        <div ref={this.mapRef} id="mapid" className="map">
-          <div id="wrapper">
-            <div id="northButton">
-              <button id="north" onClick={this.goNorth.bind(this)}>
-                North
-						</button>
-            </div>
-            <div id="map-middle">
-              <div id="westButton">
-                <button id="west" onClick={this.goWest.bind(this)}>
-                  West
+	render() {
+		return (
+			<div className="row">
+				<div ref={this.mapRef} id="mapid" className="map">
+					<div id="wrapper">
+						<div id="northButton">
+							<button
+								id="north"
+								onClick={this.goNorth.bind(this)}
+							>
+								North
 							</button>
-              </div>
-              <div id="map">
-                <div id="returnButton">
-                  <button id="return" onClick={this.goReturn.bind(this)}>
-                    Return
+						</div>
+						<div id="map-middle">
+							<div id="westButton">
+								<button
+									id="west"
+									onClick={this.goWest.bind(this)}
+								>
+									West
 								</button>
-                </div>
-              </div>
-              <div id="eastButton">
-                <button id="east" onClick={this.goEast.bind(this)}>
-                  East
-							</button>
-              </div>
-            </div>
-            <div className="row">
-              <div className="balancer" />
-              <div id="southButton">
-                <button id="south" onClick={this.goSouth.bind(this)}>
-                  South
-							</button>
-              </div>
-              <div id="county-image">
-                <button id="countiesButton" onClick={this.randomCounty.bind(this)}>
-                  Start
-							</button>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div className="column">
-          <div id="status">
-            <div className="bold">
-              GeoVermonter
-          </div>
-          </div>
-          <button>Addison County</button>
-          <button>Bennington County</button>
-          <button>Caledonia County</button>
-          <button>Chittenden County</button>
-          <button>Essex County</button>
-          <button>Franklin County</button>
-          <button>Grand Isle County</button>
-          <button>Lamoille County</button>
-          <button>Orange County</button>
-          <button>Orleans County</button>
-          <button>Rutland County</button>
-          <button>Washington County</button>
-          <button>Windham County</button>
-          <button>Windsor County</button>
-        </div>
-      </div>
-    );
-  }
+							</div>
+							<div id="map">
+								<div id="returnButton">
+									<button
+										id="return"
+										onClick={this.goReturn.bind(this)}
+									>
+										Return
+									</button>
+								</div>
+							</div>
+							<div id="eastButton">
+								<button
+									id="east"
+									onClick={this.goEast.bind(this)}
+								>
+									East
+								</button>
+							</div>
+						</div>
+						<div className="row">
+							<div className="balancer" />
+							<div id="southButton">
+								<button
+									id="south"
+									onClick={this.goSouth.bind(this)}
+								>
+									South
+								</button>
+							</div>
+							<div id="county-image">
+								<button
+									id="countiesButton"
+									onClick={this.randomCounty.bind(this)}
+								>
+									Start
+								</button>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div className="column">
+					<div id="status">
+						<div className="bold">GeoVermonter</div>
+						<div>
+							<WarningBanner warn={this.state.showWarning} />
+						</div>
+					</div>
+					<button onClick={this.handleToggleClick}>
+						Addison County
+					</button>
+					<button>Bennington County</button>
+					<button>Caledonia County</button>
+					<button>Chittenden County</button>
+					<button>Essex County</button>
+					<button>Franklin County</button>
+					<button>Grand Isle County</button>
+					<button>Lamoille County</button>
+					<button>Orange County</button>
+					<button>Orleans County</button>
+					<button>Rutland County</button>
+					<button>Washington County</button>
+					<button>Windham County</button>
+					<button>Windsor County</button>
+				</div>
+			</div>
+		);
+	}
 }
 const rootElement = document.getElementById('root');
 ReactDOM.render(<Livemap />, rootElement);
